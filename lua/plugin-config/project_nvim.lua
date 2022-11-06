0a49fca630a9c1b297ec9198916429bf9d38249b
Tjisse van der Wal <T.vanderWal@cjib.nl>
2022-11-06 19:45:46 +0100

    Fix project roots

--- lua/plugin-config/project_nvim.lua
+++ lua/plugin-config/project_nvim.lua
@@ -1,4 +1,5 @@
 require('telescope').load_extension('projects')
 require('project_nvim').setup {
-  patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.sln', 'deps.edn', 'project.clj', 'pom.xml', 'build.gradle' },
+  patterns = { '!=service', '!=itest', '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.sln', 'deps.edn', 'project.clj', 'pom.xml', 'build.gradle' },
+  ignore_lsp = {'jdtls'},
 }
