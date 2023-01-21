20cb447d822ffd4bb76a99b3115de777526b50c4
Tjisse van der Wal <tjissevdwal@gmail.com>
2023-01-21 13:42:27 +0100

    Packer to Lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,157 +1,200 @@
-require('packer').startup(function(use)
-  use { 'Mofiqul/vscode.nvim', config = function() require('plugin-config.vscode_nvim') end }
-  use {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {
+    'Mofiqul/vscode.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function() require('plugin-config.vscode_nvim') end
+  },
+  {
     'NTBBloodbath/rest.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function() require('rest-nvim').setup() end,
-  }
-  use 'Olical/aniseed'
-  use { 'Olical/conjure', config = function() require('plugin-config.conjure') end }
-  use 'PeterRincker/vim-argumentative'
-  use {
+  },
+  'Olical/aniseed',
+  {
+    'Olical/conjure',
+    ft = { 'clojure', 'fennel', 'janet', 'racket', 'hy', 'scheme', 'guile', 'commonlisp', 'julia', 'rust', 'lua',
+      'python' },
+    config = function() require('plugin-config.conjure') end
+  },
+  'PeterRincker/vim-argumentative',
+  {
     'TimUntersberger/neogit',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function () require('plugin-config.neogit') end
-  }
-  use {
+    cmd = 'Neogit',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function() require('plugin-config.neogit') end
+  },
+  {
     'abecodes/tabout.nvim',
     config = function() require('plugin-config.tabout_nvim') end,
-    wants = { 'nvim-treesitter' },
-    after = { 'nvim-autopairs' }
-  }
-  use { 'ahmedkhalf/project.nvim', config = function() require('plugin-config.project_nvim') end }
-  use 'chaoren/vim-wordmotion'
-  use {
+    dependencies = { 'windwp/nvim-autopairs' },
+  },
+  { 'ahmedkhalf/project.nvim', config = function() require('plugin-config.project_nvim') end },
+  'chaoren/vim-wordmotion',
+  { 'chomosuke/term-edit.nvim', tag = 'v1.1.0' },
+  {
     "danymat/neogen",
     config = function() require('plugin-config.neogen') end,
-    requires = "nvim-treesitter/nvim-treesitter",
-  }
-  use { 'derekwyatt/vim-fswitch', config = function() require('plugin-config.vim-fswitch') end }
-  use 'dsych/blanket.nvim'
-  use { 'gbprod/yanky.nvim', config = function() require('plugin-config.yanky_nvim') end }
-  use { 'gbprod/substitute.nvim', config = function() require('plugin-config.substitute_nvim') end }
-  use { 'gbrlsnchs/telescope-lsp-handlers.nvim', config = function() require('telescope').load_extension('lsp_handlers') end }
-  use { 'ggandor/leap.nvim', config = function() require('leap').add_default_mappings() end }
-  use 'farmergreg/vim-lastplace'
-  use 'folke/neodev.nvim'
-  use { 'folke/which-key.nvim', config = function() require('plugin-config.which-key_nvim') end }
-  use {
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  { 'derekwyatt/vim-fswitch', config = function() require('plugin-config.vim-fswitch') end },
+  'dsych/blanket.nvim',
+  { 'gbprod/yanky.nvim', config = function() require('plugin-config.yanky_nvim') end },
+  { 'gbprod/substitute.nvim', config = function() require('plugin-config.substitute_nvim') end },
+  { 'gbrlsnchs/telescope-lsp-handlers.nvim', config = function() require('telescope').load_extension('lsp_handlers') end },
+  { 'ggandor/leap.nvim', config = function() require('leap').add_default_mappings() end },
+  'farmergreg/vim-lastplace',
+  { 'folke/which-key.nvim', lazy = true, config = function() require('plugin-config.which-key_nvim') end },
+  {
     'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
     config = function()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
+      require 'alpha'.setup(require 'alpha.themes.startify'.config)
     end
-  }
-  use 'hardikpthv/vscode-wc-snippets'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lsp-document-symbol'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-path'
-  use { 'hrsh7th/nvim-cmp', config = function() require('plugin-config.nvim-cmp') end, run = ':TSUpdate' }
-  use 'hrsh7th/cmp-vsnip'
-  use { 'hrsh7th/vim-vsnip', config = function() require('plugin-config.vim-vsnip') end }
-  use 'hrsh7th/vim-vsnip-integ'
-  use 'inkarkat/vim-ExtractMatches'
-  use 'inkarkat/vim-ingo-library'
-  use {
+  },
+  'hardikpthv/vscode-wc-snippets',
+  {
+    'hrsh7th/nvim-cmp',
+    event = "InsertEnter",
+    config = function() require('plugin-config.nvim-cmp') end,
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-document-symbol',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-vsnip',
+      'kristijanhusak/vim-dadbod-completion',
+      { 'hrsh7th/vim-vsnip', config = function() require('plugin-config.vim-vsnip') end },
+      'hrsh7th/vim-vsnip-integ',
+    },
+    run = ':TSUpdate',
+  },
+  'inkarkat/vim-ExtractMatches',
+  'inkarkat/vim-ingo-library',
+  {
     'nvim-neotest/neotest',
-    requires = {
+    dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
       'nvim-neotest/neotest-vim-test',
       'vim-test/vim-test'
     },
     config = function() require('plugin-config.nvim-neotest') end,
-  }
-  use {
+  },
+  {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
     config = function() require('plugin-config.lualine_nvim') end,
-    after = { 'vscode.nvim' },
-  }
-  use {
+  },
+  {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function() require('plugin-config.telescope_nvim') end,
-  }
-  use { 'nvim-telescope/telescope-file-browser.nvim', config = function() require('telescope').load_extension('file_browser') end }
-  use { 'nvim-telescope/telescope-fzy-native.nvim', config = function() require('telescope').load_extension('fzy_native') end }
-  use { 'nvim-telescope/telescope-ui-select.nvim', config = function() require('telescope').load_extension('ui-select') end }
-  use 'kevinhwang91/nvim-hlslens'
-  use { 'kovisoft/paredit', config = function() require('plugin-config.paredit') end }
-  use 'kristijanhusak/vim-dadbod-completion'
-  use 'kristijanhusak/vim-dadbod-ui'
-  use {
+  },
+  { 'nvim-telescope/telescope-file-browser.nvim',
+    config = function() require('telescope').load_extension('file_browser') end },
+  { 'nvim-telescope/telescope-fzy-native.nvim', config = function() require('telescope').load_extension('fzy_native') end },
+  { 'nvim-telescope/telescope-ui-select.nvim', config = function() require('telescope').load_extension('ui-select') end },
+  'kevinhwang91/nvim-hlslens',
+  { 'kovisoft/paredit', config = function() require('plugin-config.paredit') end },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    cmd = { 'DBUI', 'DBUIToggle' },
+    dependencies = {
+      { 'tpope/vim-dadbod', config = function() require('plugin-config/vim-dadbod') end },
+    },
+  },
+  {
     'kyazdani42/nvim-web-devicons',
     config = function() require('nvim-web-devicons') end,
-    after = { 'vscode.nvim' },
-  }
-  use { 'kyoh86/telescope-windows.nvim', config = function() require('telescope').load_extension('windows') end }
-  use { 'liuchengxu/vim-better-default', config = function() require('plugin-config.vim-better-default') end }
-  use {
+    dependencies = { 'Mofiqul/vscode.nvim' },
+  },
+  { 'kyoh86/telescope-windows.nvim', config = function() require('telescope').load_extension('windows') end },
+  { 'liuchengxu/vim-better-default', config = function() require('plugin-config.vim-better-default') end },
+  {
     'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function() require('gitsigns').setup() end,
-  }
-  -- use 'lewis6991/impatient.nvim'
-  use {
+  },
+  {
     "Maan2003/lsp_lines.nvim",
     config = function() require("lsp_lines").setup() end,
-    after = 'nvim-lspconfig',
-  }
-  use {
+    dependencies = { 'neovim/nvim-lspconfig' },
+  },
+  {
     'mfussenegger/nvim-dap',
+    cmd = 'DapContinue',
     config = function() require('plugin-config.nvim-dap') end,
-    requires = {
+    dependencies = {
       'theHamsta/nvim-dap-virtual-text',
       'rcarriga/nvim-dap-ui',
       'jbyuki/one-small-step-for-vimkind'
-    }
-  }
-  use 'mfussenegger/nvim-jdtls'
-  use { 'mfussenegger/nvim-lint', config = function() require('plugin-config.nvim-lint') end }
-  use {
+    },
+  },
+  'mfussenegger/nvim-jdtls',
+  { 'mfussenegger/nvim-lint', config = function() require('plugin-config.nvim-lint') end },
+  {
     'neovim/nvim-lspconfig',
     config = function() require('plugin-config.nvim-lspconfig') end,
-    after = { 'tabout.nvim', 'mason-lspconfig.nvim' },
-  }
-  use { 'nvim-treesitter/nvim-treesitter', config = function() require('plugin-config.nvim-treesitter') end }
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use 'nvim-treesitter/playground'
-  use 'onsails/lspkind.nvim'
-  use { 'petertriho/nvim-scrollbar', config = function() require('plugin-config.nvim-scrollbar') end }
-  use 'psliwka/vim-smoothie'
-  use { 'qpkorr/vim-bufkill', config = function() require('plugin-config.vim-bufkill') end }
-  use 'radenling/vim-dispatch-neovim'
-  use { 'rmagatti/auto-session', config = function() require('plugin-config.auto-session') end }
-  use { 'rrethy/vim-hexokinase', run = 'make hexokinase', config = function() require('plugin-config.vim-hexokinase') end }
-  use {
+    dependencies = {
+      'abecodes/tabout.nvim',
+      'folke/neodev.nvim',
+      'williamboman/mason-lspconfig.nvim'
+    },
+  },
+  { 'nvim-treesitter/nvim-treesitter', config = function() require('plugin-config.nvim-treesitter') end },
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  'nvim-treesitter/playground',
+  'onsails/lspkind.nvim',
+  { 'petertriho/nvim-scrollbar', config = function() require('plugin-config.nvim-scrollbar') end },
+  'psliwka/vim-smoothie',
+  { 'qpkorr/vim-bufkill', config = function() require('plugin-config.vim-bufkill') end },
+  'radenling/vim-dispatch-neovim',
+  { 'rmagatti/auto-session', config = function() require('plugin-config.auto-session') end },
+  { 'rrethy/vim-hexokinase', run = 'make hexokinase', config = function() require('plugin-config.vim-hexokinase') end },
+  {
     'scalameta/nvim-metals',
-    requires = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
+    ft = { 'scala', 'sbt' },
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap'
+    },
     config = function() require('plugin-config.nvim-metals') end,
-    after = { 'nvim-lspconfig' }
-  }
-  use 'simnalamburt/vim-mundo'
-  use { 'sindrets/diffview.nvim', config = function() require('plugin-config.diffview_nvim') end }
-  use 'seudev/vscode-java-snippets'
-  use 'stefandtw/quickfix-reflector.vim'
-  use { 'stevearc/qf_helper.nvim', config = function() require('qf_helper').setup() end }
-  use 'tpope/vim-commentary'
-  use { 'tpope/vim-dadbod', config = function() require('plugin-config/vim-dadbod') end }
-  use 'tpope/vim-dispatch'
-  use 'tpope/vim-eunuch'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-surround'
-  use 'wbthomason/packer.nvim'
-  use 'wesleyegberto/vscode-java-tests'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use { 'windwp/nvim-autopairs', config = function() require('plugin-config.nvim-autopairs') end }
-  use 'windwp/nvim-ts-autotag'
-end)
-
-local packerUserConfig = vim.api.nvim_create_augroup('packer_user_config', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', { pattern = 'plugins.lua', command = 'source <afile> | PackerCompile', group = packerUserConfig })
+  },
+  'simnalamburt/vim-mundo',
+  {
+    'sindrets/diffview.nvim',
+    cmd = 'DiffviewOpen',
+    config = function() require('plugin-config.diffview_nvim') end,
+  },
+  'seudev/vscode-java-snippets',
+  'stefandtw/quickfix-reflector.vim',
+  { 'stevearc/qf_helper.nvim', config = function() require('qf_helper').setup() end },
+  'tpope/vim-commentary',
+  'tpope/vim-dispatch',
+  'tpope/vim-eunuch',
+  'tpope/vim-repeat',
+  'tpope/vim-sleuth',
+  'tpope/vim-surround',
+  'wesleyegberto/vscode-java-tests',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  { 'windwp/nvim-autopairs', config = function() require('plugin-config.nvim-autopairs') end },
+  'windwp/nvim-ts-autotag',
+})
