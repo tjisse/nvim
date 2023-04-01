c3be13aa4b311a064080a745accf2928e33785af
Tjisse van der Wal <tjissevdwal@gmail.com>
2023-04-01 15:08:56 +0200

    Disable diagnostics for Conjure log buffer

--- lua/plugin-config/conjure.lua
+++ lua/plugin-config/conjure.lua
@@ -3,3 +3,5 @@ vim.g['conjure#mapping#doc_word'] = false
 vim.g['conjure#extract#tree_sitter#enabled'] = true
 vim.g['conjure#client#clojure#nrepl#refresh#after'] = 'user/restart'
 vim.g['conjure#log#wrap'] = true
+
+vim.api.nvim_create_autocmd('BufNewFile', { pattern = 'conjure-log-*', command = 'lua vim.diagnostic.disable(0)' })
