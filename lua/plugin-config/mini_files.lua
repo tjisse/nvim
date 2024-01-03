957d94d93cc7d8b500afe05f8029d29f004c7300
Tjisse van der Wal <T.vanderWal@cjib.nl>
2024-01-03 23:22:08 +0100

    Close mini_files on <CR>

--- lua/plugin-config/mini_files.lua
+++ lua/plugin-config/mini_files.lua
@@ -9,7 +9,7 @@ mini_files.setup({
 vim.api.nvim_create_autocmd('User', {
   pattern = 'MiniFilesBufferCreate',
   callback = function(args)
-    vim.keymap.set('n', '<CR>', mini_files.go_in, { buffer = args.data.buf_id })
+    vim.keymap.set('n', '<CR>', function() mini_files.go_in({ close_on_file = true }) end, { buffer = args.data.buf_id })
     vim.keymap.set('n', '-', mini_files.go_out, { buffer = args.data.buf_id })
   end,
 })
