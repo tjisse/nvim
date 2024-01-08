1db1d86365b5c37e597408a38970374923858e6b
Tjisse van der Wal <tjissevdwal@gmail.com>
2024-01-08 20:38:23 +0100

    Update Conjure config

--- lua/plugin-config/conjure.lua
+++ lua/plugin-config/conjure.lua
@@ -1,7 +1,8 @@
 vim.g['conjure#completion#omnifunc'] = ''
 vim.g['conjure#mapping#doc_word'] = false
 vim.g['conjure#extract#tree_sitter#enabled'] = true
-vim.g['conjure#client#clojure#nrepl#refresh#after'] = 'user/restart'
+vim.g['conjure#client#clojure#nrepl#refresh#before'] = 'mount/stop'
+vim.g['conjure#client#clojure#nrepl#refresh#after'] = 'mount/start'
 vim.g['conjure#log#wrap'] = true
 
 vim.api.nvim_create_autocmd('BufNewFile', { pattern = 'conjure-log-*', command = 'lua vim.diagnostic.disable(0)' })
