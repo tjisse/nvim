e1945e7cea8f2fb7fb1ba94b825025aafd66d0a8
Tjisse van der Wal <tjissevdwal@gmail.com>
2024-01-28 13:32:51 +0100

    Use zprint for Clojure formatting

--- lua/plugin-config/conform_nvim.lua
+++ lua/plugin-config/conform_nvim.lua
@@ -1,6 +1,6 @@
 require('conform').setup({
   formatters_by_ft = {
-    clojure = { 'cljstyle' },
+    clojure = { 'zprint' },
   },
   init = function()
     vim.o.formatexpr = 'v:lua.require"conform".formatexpr()'
