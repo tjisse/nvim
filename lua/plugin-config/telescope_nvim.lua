97bb1a225dbeed35c33b59d53fab97643a47a350
Tjisse van der Wal <T.vanderWal@cjib.nl>
2024-08-24 18:24:54 +0200

    Find workspace symbols

--- lua/plugin-config/telescope_nvim.lua
+++ lua/plugin-config/telescope_nvim.lua
@@ -3,18 +3,19 @@ local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 local mini_files = require('mini.files')
 
-telescope.load_extension('lsp_handlers')
-telescope.load_extension('fzf')
-telescope.load_extension('ui-select')
-telescope.load_extension('windows')
-telescope.load_extension('recent-files')
-
 local projects_path
 if vim.fn.has('win32') then
   projects_path = 'D:/work/Projects'
 else
   projects_path = '~/Projects'
 end
+
+local fzf_opts = {
+  fuzzy = true,
+  override_generic_sorter = true,
+  override_file_sorter = true,
+  case_mode = "smart_case",
+}
 
 telescope.setup({
   defaults = {
@@ -54,10 +55,23 @@ telescope.setup({
       ignore_current_buffer = true,
       sort_mru = true,
     },
+    lsp_dynamic_workspace_symbols = {
+      sorter = telescope.extensions.fzf.native_fzf_sorter(fzf_opts)
+    },
+  },
+  extensions = {
+    fzf = fzf_opts
   },
 })
 
+telescope.load_extension('lsp_handlers')
+telescope.load_extension('fzf')
+telescope.load_extension('ui-select')
+telescope.load_extension('windows')
+telescope.load_extension('recent-files')
+
 vim.keymap.set('n', '-', function() mini_files.open(vim.api.nvim_buf_get_name(0)) end, { silent = true, desc = 'open directory of current file' })
+vim.keymap.set('n', 'gw', function() builtin.lsp_dynamic_workspace_symbols() end, { silent = true, desc = 'find workspace symbols' })
 vim.keymap.set('n', '<Space>fd', function() builtin.find_files({ cwd = vim.fn.expand('%:p:h') }) end, { silent = true, desc = 'files in current directory' })
 vim.keymap.set('n', '<Space>fr', ':Telescope oldfiles<CR>', { silent = true, desc = 'recent files' })
 vim.keymap.set('n', '<Space>bb', builtin.buffers, { silent = true, desc = 'open buffers' })
@@ -68,7 +82,6 @@ vim.keymap.set('n', '<Space>gc', builtin.git_bcommits, { silent = true, desc = '
 vim.keymap.set('n', '<Space>pd', builtin.diagnostics, { silent = true, desc = 'show diagnostics in project' })
 vim.keymap.set('n', '<Space>pf', function() telescope.extensions['recent-files'].recent_files({ hidden = true, include_current_file = false }) end, { silent = true, desc = 'find file in project' })
 vim.keymap.set('n', '<Space>pp', telescope.extensions.projects.projects, { silent = true, desc = 'find project' })
-vim.keymap.set('n', '<Space>pl', builtin.lsp_dynamic_workspace_symbols, { silent = true, desc = 'search lsp symbol in project' })
 vim.keymap.set('n', '<Space>ps', builtin.live_grep, { silent = true, desc = 'search in project' })
 vim.keymap.set('n', '<Space>pS', builtin.grep_string, { silent = true, desc = 'search word under cursor in project' })
 vim.keymap.set('n', '<Space>wf', telescope.extensions.windows.list, { silent = true, desc = 'find window' })
