8e4ccc1500231914b52897e7561cf9a5627d29d9
Tjisse van der Wal <T.vanderWal@cjib.nl>
2024-11-04 10:14:09 +0100

    Add IntelliJ formatter for java

--- lua/plugin-config/conform_nvim.lua
+++ lua/plugin-config/conform_nvim.lua
@@ -1,6 +1,18 @@
 require('conform').setup({
+  formatters = {
+    intellij = {
+      command = 'idea.bat',
+      require_cwd = false,
+      stdin = false,
+      args = {
+        'format',
+        '$FILENAME',
+      },
+    },
+  },
   formatters_by_ft = {
     clojure = { 'zprint' },
+    java = { 'intellij' },
   },
   init = function()
     vim.o.formatexpr = 'v:lua.require"conform".formatexpr()'
