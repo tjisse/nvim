4e6c55154e19ba1f82fef8651205cf680a03efbf
Tjisse van der Wal <tjissevdwal@gmail.com>
2025-03-29 15:51:10 +0100

    Update mappings

--- lua/plugins/snacks_nvim.lua
+++ lua/plugins/snacks_nvim.lua
@@ -105,8 +105,8 @@ return {
     { "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
     { "gI",              function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
     { "gy",              function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
-    { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
-    { "<leader>sS",      function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
+    { "<leader>sS",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
+    { "<leader>ss",      function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
     -- Other
     { "<leader>z",       function() Snacks.zen() end,                                            desc = "Toggle Zen Mode" },
     { "<leader>Z",       function() Snacks.zen.zoom() end,                                       desc = "Toggle Zoom" },
