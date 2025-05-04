35ebb439823229c8548ed47be9a392cb0e9dd916
Tjisse van der Wal <tjissevdwal@gmail.com>
2025-05-04 17:19:56 +0200

    Update conform.nvim config

--- lua/plugins/conform_nvim.lua
+++ lua/plugins/conform_nvim.lua
@@ -4,20 +4,10 @@ return {
   cmd = { 'ConformInfo' },
   config = function()
     require('conform').setup({
-      formatters = {
-        intellij = {
-          command = 'idea.bat',
-          require_cwd = false,
-          stdin = false,
-          args = {
-            'format',
-            '$FILENAME',
-          },
-        },
-      },
       formatters_by_ft = {
         clojure = { 'zprint' },
-        java = { 'intellij' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
       },
       init = function()
         vim.o.formatexpr = 'v:lua.require"conform".formatexpr()'
