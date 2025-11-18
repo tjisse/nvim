3e6bae362b68df0b2d7e29e2654719c4c8142b21
Tjisse van der Wal <tjissevdwal@gmail.com>
2025-11-18 19:46:50 +0100

    Remove copilot_lua, update nvim-java versions

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,5 +1,18 @@
 return {
   'nvim-java/nvim-java',
   lazy = true,
-  opts = {},
+  opts = {
+    jdtls = {
+      version = 'v1.52.0',
+    },
+    java_test = {
+      version = '0.43.2',
+    },
+    java_debug_adapter = {
+      version = '0.58.2',
+    },
+    spring_boot_tools = {
+      version = '1.59.0',
+    },
+  },
 }
